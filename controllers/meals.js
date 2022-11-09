46986cd70545f7b4972a93d4edd4e47209d76ad6
rmap88 <ronnepdev@gmail.com>
2022-11-08 17:05:02 -0800

    add edit functionality

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,12 +1,14 @@
 var Meal = require("../models/meal");
-var User = require("../models/user")
+var User = require("../models/user");
 
 module.exports = {
   index,
   show,
   new: newMeal,
   create,
-  delete: deleteMeal
+  delete: deleteMeal,
+  edit,
+  update
 };
 
 function index(req, res) {
@@ -40,9 +42,31 @@ function deleteMeal(req, res) {
   );
 }
 
+function edit(req, res) {
+  Meal.findOne({_id: req.params.id, user: req.user._id}, function(err, meal) {
+    if (err || !meal) return res.redirect('/meals');
+    res.render('meals/edit', { meal, title: "Edit Meal" });
+  });
+}
+
+function update(req, res) {
+  Meal.findOneAndUpdate(
+    {_id: req.params.id, user: req.user._id},
+    // update object with updated properties
+    req.body,
+    // options object with new: true to make sure updated doc is returned
+    {new: true},
+    function(err, meal) {
+      if (err || !meal) return res.redirect('/meals');
+      res.redirect(`/meals/${meal._id}`);
+    }
+  );
+}
+
+
 function create(req, res) {
   req.body.user = req.user._id;
-  console.log(req.body)
+  console.log(req.body);
   var meal = new Meal(req.body);
  
   meal.save(function (err) {
--- routes/meals.js
+++ routes/meals.js
@@ -6,7 +6,9 @@ const mealsCtrl = require('../controllers/meals');
 router.get('/', mealsCtrl.index);
 router.get('/new', mealsCtrl.new);
 router.get('/:id', mealsCtrl.show);
+router.get('/:id/edit', mealsCtrl.edit);
 router.post('/', mealsCtrl.create);
-router.delete("/:id", mealsCtrl.delete); 
+router.delete("/:id", mealsCtrl.delete);
+router.put('/:id', mealsCtrl.update);
 
 module.exports = router;
